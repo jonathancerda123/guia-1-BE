125a95e8f4d24c3a11edb963e410d713788a7181
valentinaromans <valentina.roman03@inacapmail.cl>
2024-09-03 15:57:07 -0400

    segundavista2

--- valony2/urls.py
+++ valony2/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.bienvenida, name='bienvenida'),
+    path('', views.bienvenida), #la primera parte es el enlace.
+    path('estado/', views.estado),
 ]
--- valony2/views.py
+++ valony2/views.py
@@ -3,4 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def bienvenida(request):
-    return HttpResponse("¡Hola! Bienvenido/a esta es la página de inicio.")
+    html="""¡Hola! Bienvenido/a, esta es la página de inicio.
+            <strong><a href="/valony2/estado">Siguiente</a></strong>
+    """
+    return HttpResponse(html)
+
+def estado(request):
+    html="""
+    <h1>¿Cómo estás?</h1>
+    <body>Espero que bien, ¡Ten un lindo día!</body>
+    """
+    return HttpResponse(html)

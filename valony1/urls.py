f21e1944d09e21cff879c54b4ecb6c7c6acda727
jonathancerda123 <jonathanygnaciocerdagarrido@gmail.com>
2024-09-03 23:08:31 -0400

    aplicacion terminada 2

--- valony1/urls.py
+++ valony1/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('1/', views.index, name='index'),
-    path('2/', views.comienzo, name='comienzo'),
+    path('1/', views.index),
+    path('2/', views.comienzo),
 ]
